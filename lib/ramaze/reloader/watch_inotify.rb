b43e338d971a38427ff6c91328fe29ebca57f04e
Aki Reijonen <aki.reijonen@gmail.com>
2008-12-27 12:08:58 +0200

    make inotify watcher monitor directories instead of individual files

--- lib/ramaze/reloader/watch_inotify.rb
+++ lib/ramaze/reloader/watch_inotify.rb
@@ -14,9 +14,10 @@ module Ramaze
 
     class WatchInotify
       POLL_INTERVAL = 2 # seconds
-      NOTIFY_MASK = RInotify::MODIFY | RInotify::IN_ONESHOT
+      NOTIFY_MASK = RInotify::CREATE | RInotify::MOVED_TO | RInotify::CLOSE_WRITE | RInotify::MODIFY
 
       def initialize
+        @watched_files = Set.new
         @watcher = RInotify.new
         @changed = Queue.new
         @watcher_thread = start_watcher
@@ -38,25 +39,32 @@ module Ramaze
         return unless @watcher.wait_for_events(POLL_INTERVAL)
 
         @watcher.each_event do |event|
-          @changed.push(event.watch_descriptor)
+          # [directory_descriptor, filename] 
+          @changed.push([event.watch_descriptor, event.name])
         end
       end
 
       def watch(file)
-        return if @watcher.watch_descriptors.has_value?(file)
-        return unless File.exist?(file)
-
-        @watcher.add_watch(file, NOTIFY_MASK)
+        return if @watched_files.include?(file)
+        return if not File.exist?(file)
+        @watched_files << file
+        dirname = File.dirname(file)
+        return if @watcher.watch_descriptors.has_value?(dirname)
+        @watcher.add_watch(dirname, NOTIFY_MASK)
       rescue Errno::ENOENT
         retry
       end
 
-      # FIXME:
-      #   Seems like this won't work due to some bug in the rinotify library.
-      #   Would be cool if someone could make a FFI version.
-
       def remove_watch(file)
-        @watcher.rm_watch(file)
+        return unless @watched_files.include?(file)
+        @watched_files.delete(file)
+        directory = File.dirname(file)
+
+        # remove directory watch if last watched file in directory
+        if not @watched_files.find {|f| File.dirname(f) == directory }
+          descriptor,* = @watcher.watch_descriptors.find {|k,v| v == directory }
+          @watcher.rm_watch(descriptor) if descriptor
+        end
       end
 
       def close
@@ -65,20 +73,16 @@ module Ramaze
         true
       end
 
-      # NOTE:
-      #   We have to add the changed file again after we got a notification, I
-      #   have no idea why, but using IN_ONESHOT should make sure that there is
-      #   no memory leak in the C level even if we add a file again.
-      #   There is a memory leak however in the watch_descriptors hash, since
-      #   rinotify won't synchronize the contents properly and will only add to
-      #   the hash, so we have to clean up ourselves.
       def changed_files
+        files = Set.new
         until @changed.empty?
-          descriptor = @changed.shift
-          file = @watcher.watch_descriptors.delete(descriptor)
-          watch(file)
-          yield(file)
+          directory_descriptor, file = @changed.shift
+          directory = @watcher.watch_descriptors[directory_descriptor]
+          path = "#{directory}/#{file}"
+          # can't yield yet, could have duplicates
+          files << path if @watched_files.include? path
         end
+        files.each {|f| yield f }
       end
     end
   end
